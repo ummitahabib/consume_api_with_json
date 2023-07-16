edfc9c6f58e6087cf4fe597fea34a42c9b7cff4f
ummitahabib <ummihabib88@gmail.com>
2023-07-16 19:17:58 +0100

    run build_runner

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,6 +1,7 @@
 import 'package:consume_json_placeholder/user.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
+part 'api_service.g.dart';
 
 @RestApi(baseUrl: "https://portal.airsmat.com")
 abstract class ApiService{
